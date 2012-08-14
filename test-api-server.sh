fa60277032d802d67069d9232245c7084b5706e2
Stanislav Tugovikov <s.tugovikov@corp.mail.ru>
2012-08-14 17:21:06 +0400

    rename update mpop; remove obsolete test scripts

--- test-api-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget "http://e.mail.ru/api/v1/ab?token=123456789&email=andrewsumin@corp.mail.ru" -O result && cat result && rm result
--- test-fest-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget "http://127.0.0.1:8090/$1" --header='X-Fest-Dir: /usr/local/www/mail.ru.deploy/mail.ru.tugovikov/data/ru/' -O result 2>/dev/null&& cat result && rm result
