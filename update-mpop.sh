efb25f7d821e0aaaa25b6c1e5b3e81e85acb3138
Stanislav Tugovikov <s.tugovikov@corp.mail.ru>
2012-08-14 17:10:31 +0400

    maximov fixes

--- update-mpop.sh
+++ update-mpop.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
 
 reset() {
+	git reset
 	git checkout -- .
 }
-
-last_rev() {
-	TYPE=$1
-	git branch -a | grep "remotes/origin/${TYPE}-" | sort  | tail -n 1 | sed 's@remotes/origin/@@' | sed -r 's/\s+//g'
-}
-
-
 
 pushd ~/mpop/ > /dev/null
 
 # на всякий случай откатим все изменения в папке
 reset
 
-# сначала нужно влить в последнюю итерацию изменения из prerelease
 git fetch
-
-LAST_ITERATION=`last_rev 'mail-iteration'`
-LAST_PRERELEASE=`last_rev 'prerelease-mail'`
-
-echo "iteration: ${LAST_ITERATION}, release: ${LAST_PRERELEASE}"
-
-git checkout $LAST_ITERATION
-
-git merge origin/$LAST_PRERELEASE
+git checkout prerelease-mail
 
 
 # запускать нужно из своей копии mpop
-#bash build_fe_utf.sh;
-#./autogen.sh;
-#./build.sh fe-linux-utf;
-./build-mail.sh
-sudo make install;
-cd -;
-/etc/init.d/httpd restart;
+./build_fe_utf.sh
+
+./autogen.sh
+./configure --enable-mail --enable-perl --enable-spamdelclient --enable-mysqlproc --enable-uri-list --enable-mail-ajax --enable-pro --enable-utf --enable-kav8 --enable-httplib --enable-kas5
+make -j 4
+
+sudo make install
+sudo /etc/init.d/httpd restart
 
 # после этого нужно откатить все изменения в папке
 reset
